7dfc5a784d630b95b60f2e59bd307c2be361bb02
DarkReal <wangluo.05201995@qq.com>
2016-08-19 16:45:42 +0800

    first change

--- app/src/main/java/com/xxw/student/fragment/titleFragment_shouye.java
+++ app/src/main/java/com/xxw/student/fragment/titleFragment_shouye.java
@@ -4,22 +4,26 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.ColorDrawable;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.CursorAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.xxw.student.MainActivity;
 import com.xxw.student.R;
+import com.xxw.student.shouye_detail.CompanySearch;
 import com.xxw.student.shouye_detail.city_select;
 import com.xxw.student.utils.LogUtils;
+import com.xxw.student.view.search_history.RecordSQLiteOpenHelper2;
 
 /**
  * 首页--头部
@@ -32,18 +36,30 @@ public class titleFragment_shouye extends Fragment {
     private View view;
     private ViewGroup mcontainer;
     private LayoutInflater minflater;
-    private mypop pop;
+
+    private PopupWindow popupWindow;
 
     private LinearLayout search_icon;
     private TextView located;
     private String current_city="";
 
-    public View onCreateView(final LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+    /*数据库变量*/
+    private RecordSQLiteOpenHelper2 helper;
+    private SQLiteDatabase db;
+    private SimpleCursorAdapter adapter;
+
+
+    private LinearLayout search_linear;
+    private ListView search_list;
+    private TextView clear_history;
+    private View content;
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,Bundle savedInstanceState){
 
 
         minflater = inflater;
         mcontainer = container;
         view=inflater.inflate(R.layout.main_title_shouye, container, false);
+
 
         SharedPreferences preferences = getActivity().getSharedPreferences("userInfo",
                 Activity.MODE_PRIVATE);
@@ -65,29 +81,16 @@ public class titleFragment_shouye extends Fragment {
 
         select_location = (LinearLayout) view.findViewById(R.id.select_location);
         search_icon = (LinearLayout) view.findViewById(R.id.search_icon);
+        //单击转到查询页面
         search_icon.setOnClickListener(new View.OnClickListener() {
-
-
-            WindowManager.LayoutParams lp = getActivity().getWindow().getAttributes();
             @Override
-            public void onClick(View v) {
-
-                View contentView = LayoutInflater.from(view.getContext()).inflate(R.layout.main_title_shouye_search, null);
-                TextView locate = (TextView) contentView.findViewById(R.id.location);
-
-                locate.setText(located.getText().toString());
-
-                pop = new mypop(contentView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
-                View rootview = LayoutInflater.from(view.getContext()).inflate(R.layout.main_title_shouye, null);
-
-                //view.invalidate();
-                pop.setBackgroundDrawable(new ColorDrawable(00000000));
-                pop.setAnimationStyle(R.style.popup_anim);
-                pop.showAtLocation(rootview, Gravity.TOP,0,45);
-                lp.alpha=0.6f;
-                getActivity().getWindow().setAttributes(lp);
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(view.getContext(), CompanySearch.class);
+                startActivity(intent);
             }
         });
+
 
         select_location.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,20 +103,5 @@ public class titleFragment_shouye extends Fragment {
         });
 
         return view;
-    }
-    public class mypop extends PopupWindow {
-        WindowManager.LayoutParams lp = getActivity().getWindow().getAttributes();
-
-        public mypop(View contentView, int width, int height, boolean focusable) {
-            super(contentView,width,height,focusable);
-
-        }
-
-        @Override
-        public void dismiss() {
-            super.dismiss();
-            lp.alpha = 1f;
-            getActivity().getWindow().setAttributes(lp);
-        }
     }
 }
